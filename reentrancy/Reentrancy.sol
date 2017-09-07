5f20de6f8d0c7e460e1fbc7c4ed2057895d68031
Josselin <josselin@trailofbits.com>
2017-09-07 09:16:08 +0100

    Improve reentrancy example

--- reentrancy/Reentrancy.sol
+++ reentrancy/Reentrancy.sol
@@ -12,6 +12,8 @@ contract Reentrance {
     }   
 
     function withdrawBalance(){
+        // send userBalance[msg.sender] ethers to msg.sender
+        // if mgs.sender is a contract, it will call its fallback function
         if( ! (msg.sender.call.value(userBalance[msg.sender])() ) ){
             throw;
         }
@@ -19,6 +21,8 @@ contract Reentrance {
     }   
 
     function withdrawBalance_fixed(){
+        // to protect against re-entrancy, the state variable
+        // has to be change before the call
         uint amount = userBalance[msg.sender];
         userBalance[msg.sender] = 0;
         if( ! (msg.sender.call.value(amount)() ) ){
@@ -27,7 +31,10 @@ contract Reentrance {
     }   
 
     function withdrawBalance_fixed_2(){
-        // transfer is safe against reentrancy
+        // send() and transfer() are safe against reentrancy
+        // they do not transfer the remaining gas
+        // and they give just enough gas to execute few instructions    
+        // in the fallback function (no further call possible)
         msg.sender.transfer(userBalance[msg.sender]);
         userBalance[msg.sender] = 0;
     }   
