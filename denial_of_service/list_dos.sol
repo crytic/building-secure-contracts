d075aa869bdbc51b5b2982a09088b2f79f352103
Ben Perez <benperez1227@gmail.com>
2018-09-19 13:59:26 -0400

    Update list_dos.sol
    fixed `send` vs `transfer` issues

--- denial_of_service/list_dos.sol
+++ denial_of_service/list_dos.sol
@@ -6,7 +6,7 @@ contract CrowdFundBad {
 
   function refundDos() public {
     for(uint i; i < refundAddresses.length; i++) {
-      require(refundAddresses[i].send(refundAmount[refundAddresses[i]]));
+      require(refundAddresses[i].transfer(refundAmount[refundAddresses[i]]));
     }
   }
 }
@@ -18,14 +18,13 @@ contract CrowdFundPull {
   function withdraw() external {
     uint refund = refundAmount[msg.sender];
     refundAmount[msg.sender] = 0;
-    msg.sender.send(refund);
+    msg.sender.transfer(refund);
   }
 }
 
 
-//This is safe against the list length being the source of block gas limit issues
-//but is not safe in the scenario where the payee takes up a lot of gas, or is that
-// not possible because send has a fixed gas limit?
+//This is safe against the list length causing out of gas issues
+//but is not safe against the payee causing the execution to revert
 contract CrowdFundSafe {
   address[] private refundAddresses;
   mapping(address => uint) public refundAmount;
@@ -34,7 +33,7 @@ contract CrowdFundSafe {
   function refundSafe() public {
     uint256 i = nextIdx;
     while(i < refundAddresses.length && msg.gas > 200000) {
-      refundAddresses[i].send(refundAmount[i]);
+      refundAddresses[i].transfer(refundAmount[i]);
       i++;
     }
     nextIdx = i;
