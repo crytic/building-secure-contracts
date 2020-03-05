adac92bd55f7aac2ffb60ea6abb73d8bb70bd16d
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2020-03-05 15:25:03 -0300

    Create magic.sol

---
+++ program-analysis/echidna/example/magic.sol
@@ -0,0 +1,13 @@
+contract C {
+  bool value_found = false;
+  function magic(uint magic_1, uint magic_2, uint magic_3, uint magic_4) public {
+    require(magic_1 == 42);
+    require(magic_2 == 129);
+    require(magic_3 == magic_4+333);
+    value_found = true;
+    return;
+  }
+  function echidna_magic_values() public returns (bool) {
+    return !value_found;
+  }
+}
