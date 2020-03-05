f5e315b1d8866784e76d2c8c2e57f232ebe8e058
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2020-03-05 15:38:36 -0300

    Update gh_action_test.sh

--- program-analysis/echidna/scripts/gh_action_test.sh
+++ program-analysis/echidna/scripts/gh_action_test.sh
@@ -3,8 +3,8 @@
 
 install_echidna(){
     pip install crytic-compile
-    wget https://github.com/crytic/echidna/releases/download/1.2.0.0/echidna-test-v1.2.0.0-Ubuntu-18.04.tar.gz
-    tar -xvf echidna-test-v1.2.0.0-Ubuntu-18.04.tar.gz
+    wget https://github.com/crytic/echidna/releases/download/v1.2.0.0/echidna-test-1.2.0.0-Ubuntu-18.04.tar.gz
+    tar -xvf echidna-test-1.2.0.0-Ubuntu-18.04.tar.gz
     sudo mv echidna-test /usr/bin/
 }
 
