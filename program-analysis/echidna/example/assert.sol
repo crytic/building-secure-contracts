2926f743c0a2a5f44a7c4af8af2068ad492966ed
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2020-03-09 13:04:20 -0300

    Create assert.sol

---
+++ program-analysis/echidna/example/assert.sol
@@ -0,0 +1,10 @@
+contract Incrementor {
+  uint private counter = 2**200;
+
+  function inc(uint val) public returns (uint){
+    uint tmp = counter;
+    counter += val;
+    assert (tmp <= counter);
+    return (counter - tmp);
+  }
+}
