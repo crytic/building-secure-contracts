5933c7ec81f082e8c9f15ae6a5b321247cca7298
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2020-03-16 13:55:55 -0300

    Create solution.sol

---
+++ program-analysis/echidna/exercises/exercise4/solution.sol
@@ -0,0 +1,35 @@
+ contract Ownership{
+    address owner = msg.sender;
+    function Owner() public {
+         owner = msg.sender;
+     }
+     modifier isOwner(){
+         require(owner == msg.sender);
+         _;
+      }
+   }
+  contract Pausable is Ownership{
+     bool is_paused;
+     modifier ifNotPaused(){
+              require(!is_paused);
+              _;
+      }
+      function paused() isOwner public{
+          is_paused = true;
+      }
+      function resume() isOwner public{
+          is_paused = false;
+      }
+   }
+   contract Token is Pausable{
+      mapping(address => uint) public balances;
+      function transfer(address to, uint value) ifNotPaused public{
+
+           assert(balances[msg.sender] - value <= balances[msg.sender]);
+           balances[msg.sender] -= value;
+           
+           assert(balances[to] + value >= balances[to]);
+           balances[to] += value;
+            
+       }
+    }
