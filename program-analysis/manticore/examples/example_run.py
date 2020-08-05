8d1e51431139516ec8d7126b95142d4d7010dfdb
feliam <felipe.andres.manzano@gmail.com>
2020-08-05 10:42:56 -0300

    Increase caller balance in mcore example

--- program-analysis/manticore/examples/example_run.py
+++ program-analysis/manticore/examples/example_run.py
@@ -1,9 +1,12 @@
+import os
 from manticore.ethereum import ManticoreEVM
 
 m = ManticoreEVM()
+filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'example.sol')
 
-user_account = m.create_account(balance=1000)
-with open('example.sol') as f:
+# Needs enough balance to pay for the gas of the initialization
+user_account = m.create_account(balance=10**10)
+with open(filename) as f:
     contract_account = m.solidity_create_contract(f, owner=user_account)
 
 symbolic_var = m.make_symbolic_value()
