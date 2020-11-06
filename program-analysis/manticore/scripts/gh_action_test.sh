772a4667119788b1090bb0ea81689d8b2a636212
Feist Josselin <josselin@trailofbits.com>
2020-11-06 16:55:18 +0100

    Update gh_action_test.sh

--- program-analysis/manticore/scripts/gh_action_test.sh
+++ program-analysis/manticore/scripts/gh_action_test.sh
@@ -62,7 +62,14 @@ test_exercise(){
 
 
 cd program-analysis/manticore
-pip install manticore[evm]
+#pip install manticore[evm]
+git clone https://github.com/trailofbits/manticore
+git checkout dev-detect-default-solver
+pip install -e ".[evm]"
+
+sudo add-apt-repository ppa:sri-csl/formal-methods -y
+sudo apt-get update
+sudo apt-get install yices2
 
 test_example example_run.py
 test_example example_throw.py
