de85d8a42d4b3800e15135f9bbb689baedc496d9
Gustavo Grieco <31542053+ggrieco-tob@users.noreply.github.com>
2022-02-11 17:55:56 +0100

    Update gh_action_test.sh

--- program-analysis/echidna/scripts/gh_action_test.sh
+++ program-analysis/echidna/scripts/gh_action_test.sh
@@ -3,8 +3,8 @@
 
 install_echidna(){
     pip install crytic-compile slither-analyzer
-    wget https://github.com/crytic/echidna/releases/download/v1.7.2/echidna-test-1.7.2-Ubuntu-18.04.tar.gz
-    tar -xf echidna-test-1.7.2-Ubuntu-18.04.tar.gz
+    wget https://github.com/crytic/echidna/releases/download/v2.0.0/echidna-test-2.0.0-Ubuntu-18.04.tar.gz
+    tar -xf echidna-test-2.0.0-Ubuntu-18.04.tar.gz
     sudo mv ./echidna-test /usr/bin/
 }
 
