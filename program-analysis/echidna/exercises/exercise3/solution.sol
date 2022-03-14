a43ae24f435248de77dc13d19702484ff222b117
Anish Naik <anishnaik@anishs-air.myfiosgateway.com>
2022-03-14 12:49:54 -0400

    Renamed property and fixed formatting

--- program-analysis/echidna/exercises/exercise3/solution.sol
+++ program-analysis/echidna/exercises/exercise3/solution.sol
@@ -4,12 +4,13 @@ import "mintable.sol";
 contract TestToken is MintableToken {
     address echidna_caller = msg.sender;
 
+    // update the constructor
     constructor() public MintableToken(10000) {
         owner = echidna_caller;
     }
 
     // add the property
-    function echidna_test_balance() public view returns (bool) {
+    function echidna_cannot_mint_more() public view returns (bool) {
         return balances[msg.sender] <= 10000;
     }
 }
--- program-analysis/echidna/exercises/exercise3/template.sol
+++ program-analysis/echidna/exercises/exercise3/template.sol
@@ -3,9 +3,9 @@ import "mintable.sol";
 contract TestToken is MintableToken {
     address echidna_caller = msg.sender;
 
-    //update the constructor
+    // update the constructor
     constructor() public {}
 
     // add the property
-    function echidna_test_balance() public view returns (bool) {}
+    function echidna_cannot_mint_more() public view returns (bool) {}
 }
