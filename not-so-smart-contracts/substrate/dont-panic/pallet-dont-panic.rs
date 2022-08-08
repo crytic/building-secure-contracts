13fe9ac60d026208bf7c96e6bc5e1e88366b9596
Anish Naik <anish.naik@trailofbits.com>
2022-08-08 15:09:19 -0400

    Code cleanup for pallet-dont-panic.rs
    Decided to add an error type so that `ensure!()` can be used to cause the out-of-bounds check

--- not-so-smart-contracts/substrate/dont-panic/pallet-dont-panic.rs
+++ not-so-smart-contracts/substrate/dont-panic/pallet-dont-panic.rs
@@ -4,7 +4,7 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*, weights::*};
+	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
 	use sp_std::prelude::*;
 
@@ -20,7 +20,7 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	/// Storage item for useful_amounts passed to the do_work function
+	/// Storage item for holding an ImportantValue
 	#[pallet::storage]
 	#[pallet::getter(fn get_val)]
 	pub(super) type ImportantValue<T: Config> = StorageValue<_, u64, ValueQuery>;
@@ -28,30 +28,38 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Emit after do_work successfully completes
+		/// Emit after val is found
 		FoundVal(T::AccountId, u64),
 	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
+    #[pallet::error]
+	pub enum Error<T> {
+		NoImportantValueFound,
+	}
+
 	#[pallet::call]
 	impl<T:Config> Pallet<T> {
 		/// Do some work
 		///
 		/// Parameters:
-		/// - `useful_amount`: A vector of u64 values that we want to store.
+		/// - `useful_amounts`: A vector of u64 values in which there is a important value.
 		///
 		/// Emits `DidWork` event when successful.
 		#[pallet::weight(10_000)]
-		pub fn do_work(
+		pub fn find_important_value(
 			origin: OriginFor<T>,
 			useful_amounts: Vec<u64>,
 		) -> DispatchResultWithPostInfo {
 			let sender = ensure_signed(origin)?;
-			if useful_amounts[0] > 0 {
-				ImportantValue::<T>::put(&useful_amounts[0]);
-			}			
+
+			ensure!(useful_amounts[0] > 1_000, <Error<T>>::NoImportantValueFound);
+			
+			// Found the important value
+			ImportantValue::<T>::put(&useful_amounts[0]);
+						
 			// Emit event
 			Self::deposit_event(Event::FoundVal(sender, useful_amounts[0]));
 			Ok(().into())
