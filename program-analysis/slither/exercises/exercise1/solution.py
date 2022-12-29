0e8d23dcd3a4b086f18ea70472851ee1c44ab4b6
bart1e <bartolwodzinski@op.pl>
2022-12-29 19:05:51 +0100

    Slither exercise 1 fixed

--- program-analysis/slither/exercises/exercise1/solution.py
+++ program-analysis/slither/exercises/exercise1/solution.py
@@ -9,6 +9,7 @@ for contract in slither.contracts:
    if coin in contract.inheritance:
       # Get the function definition  
       mint = contract.get_function_from_signature('_mint(address,uint256)')
-      # If the function was not declarer by coin, there is a bug !  
-      if mint.contract != coin:
+      # If the function was not declared by coin, there is a bug !
+      # Detect error only for contracts overriding the '_mint' function
+      if mint.contract_declarer == contract:
            print(f'Error, {contract} overrides {mint}')
