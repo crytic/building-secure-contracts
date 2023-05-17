ed0f92b9e906dce29bc5b98b8d59ffe50c8291ea
Chris Dahlheimer <chris.dahlheimer@trailofbits.com>
2023-05-17 09:29:11 -0400

    After linting

--- static/script.js
+++ static/script.js
@@ -1,3 +1,3 @@
-let script = document.createElement('hs-script-loader');
+let script = document.createElement("hs-script-loader");
 script.src = "//js.hs-scripts.com/22554992.js";
 document.body.append(script);
