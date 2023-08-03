9185af79dda71c651cfd995c42c2fd9b9d920ddc
Chris Dahlheimer <chris.dahlheimer@trailofbits.com>
2023-08-03 13:23:25 -0400

    Ran lint:format

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 var wrapper = document.createElement("div");
-wrapper.innerHTML = '\
+wrapper.innerHTML =
+  '\
 <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/22554992.js"></script>\
 ';
 document.body.appendChild(wrapper);
