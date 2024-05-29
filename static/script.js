f9263bacd7b3ef0896eabd264ebd04b8817b80c1
hwomackToB <150697619+hwomackToB@users.noreply.github.com>
2024-05-29 04:55:43 -1000

    Update static/script.js
    Co-authored-by: Emilio López <2642849+elopez@users.noreply.github.com>

--- static/script.js
+++ static/script.js
@@ -2,4 +2,8 @@ let script = document.createElement("script");
 script.src = "//js.hs-scripts.com/22554992.js";
 document.body.append(script);
 
-<script defer data-domain="secure-contracts.com" src="https://plausible.trailofbits.network/js/script.js"></script>
+let script2 = document.createElement("script");
+script2.src = "https://plausible.trailofbits.network/js/script.js";
+script2.setAttribute("data-domain", "secure-contracts.com");
+script2.setAttribute("defer", "defer");
+document.body.append(script2);
